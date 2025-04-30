360b9a7d12a9ce8bdde92960210c170f68b928a0
gabrlcj <gabrielbittencourt57@gmail.com>
2025-04-30 20:22:45 -0300

    feat: adds new bucket to aws

--- index.ts
+++ index.ts
@@ -1,11 +1,18 @@
 import * as aws from "@pulumi/aws";
 
-const bucket = new aws.s3.BucketV2("bucket-ftr", {
-    bucket: 'bucket-ftr',
+const firstBucket = new aws.s3.BucketV2("first-bucket-ftr", {
+    bucket: 'first-bucket-ftr',
     tags: {
         IaC: 'true',
     },
 });
+
+const secondBucket = new aws.s3.BucketV2("second-bucket-ftr", {
+    bucket: 'second-bucket-ftr',
+    tags: {
+        IaC: 'true',
+    },
+})
 
 const ecr = new aws.ecr.Repository("ecr-ftr", {
     name: 'ecr-ftr',
@@ -15,10 +22,13 @@ const ecr = new aws.ecr.Repository("ecr-ftr", {
     },
 });
 
-export const bucketName = bucket.id;
-export const bucketTags = bucket.tags;
-export const bucketRegion = bucket.region;
-export const bucketArn = bucket.arn;
+export const firstBucketName = firstBucket.id;
+export const firstBucketRegion = firstBucket.region;
+export const firstBucketArn = firstBucket.arn;
+
+export const secondBucketName = secondBucket.id;
+export const secondBucketRegion = secondBucket.region;
+export const secondBucketArn = secondBucket.arn;
 
 export const ecrName = ecr.id;
 export const ecrRepositoryUrl = ecr.repositoryUrl;
