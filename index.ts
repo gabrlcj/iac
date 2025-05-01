5e5a91da260bac9e8b4e76bb6e9670c35da4199d
gabrlcj <gabrielbittencourt57@gmail.com>
2025-05-01 15:03:13 -0300

    feat: removes the second bucket

--- index.ts
+++ index.ts
@@ -7,13 +7,6 @@ const firstBucket = new aws.s3.BucketV2("bucket-ftr-1", {
     },
 });
 
-const secondBucket = new aws.s3.BucketV2("bucket-ftr-2", {
-    bucket: 'bucket-ftr-2',
-    tags: {
-        IaC: 'true',
-    },
-})
-
 const ecr = new aws.ecr.Repository("ecr-ftr", {
     name: 'ecr-ftr',
     imageTagMutability: 'IMMUTABLE',
@@ -25,10 +18,6 @@ const ecr = new aws.ecr.Repository("ecr-ftr", {
 export const firstBucketName = firstBucket.id;
 export const firstBucketRegion = firstBucket.region;
 export const firstBucketArn = firstBucket.arn;
-
-export const secondBucketName = secondBucket.id;
-export const secondBucketRegion = secondBucket.region;
-export const secondBucketArn = secondBucket.arn;
 
 export const ecrName = ecr.id;
 export const ecrRepositoryUrl = ecr.repositoryUrl;
