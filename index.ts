700a805b7ba1f079be05d1889ff6153ecd2d082b
gabrlcj <gabrielbittencourt57@gmail.com>
2025-05-01 15:38:00 -0300

    feat: adds second bucket

--- index.ts
+++ index.ts
@@ -7,6 +7,13 @@ const firstBucket = new aws.s3.BucketV2("bucket-ftr-1", {
     },
 });
 
+const secondBucket = new aws.s3.BucketV2("bucket-ftr-2", {
+    bucket: 'bucket-ftr-2',
+    tags: {
+        IaC: 'true',
+    },
+})
+
 const ecr = new aws.ecr.Repository("ecr-ftr", {
     name: 'ecr-ftr',
     imageTagMutability: 'IMMUTABLE',
@@ -18,6 +25,10 @@ const ecr = new aws.ecr.Repository("ecr-ftr", {
 export const firstBucketName = firstBucket.id;
 export const firstBucketRegion = firstBucket.region;
 export const firstBucketArn = firstBucket.arn;
+
+export const secondBucketName = secondBucket.id;
+export const secondBucketRegion = secondBucket.region;
+export const secondBucketArn = secondBucket.arn;
 
 export const ecrName = ecr.id;
 export const ecrRepositoryUrl = ecr.repositoryUrl;
